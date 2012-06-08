29d2fe09ebb7b599a96444acfdc99d400ee76dcd
Trevor Kimenye <trevor@kimenye.com>
2012-06-08 06:33:18 +0300

    Added images

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,17 @@
 function App()  {
     var self = this;
 
-    this.title = ko.observable("Prezzy");
+    self.title = ko.observable("Prezzy");
+    self.searchTerm = ko.observable();
+
+    self.presentations = ko.observableArray([
+        { id: 1000, by: "Jack Johnson", title: "Mobile First Development", at: "Melbourne Convention Hall Room 31"},
+        { id: 2000, by: "Stuart Little", title: "How to avoid those pesky cats", at: "KICC Hall 21"},
+        { id: 3000, by: "Chuck Norris", title: "How to kick ass in Development", at: "Chuck\'s dojo"}
+    ]);
+
+//    self.results = ko.computed()
+
 
     Sammy(function() {
 
